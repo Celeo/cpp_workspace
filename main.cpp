0c76034515c715cfcd82da425d07e6892a3eb688
Celeo <mattboulanger@fastmail.com>
2025-08-25 19:21:37 -0700

    "Frist program" exercise

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int main() {
-  int a = 1000000;
-  std::cout << "a is " << a << std::endl;
+  int n{0};
+  std::cout << "Enter a number: ";
+  std::cin >> n;
+  std::cout << "Double that number is: " << n * 2 << "\n";
+  std::cout << "Triple that number is: " << n * 3 << std::endl;
 
   return 0;
 }
